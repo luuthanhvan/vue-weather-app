2ec12d2a642566212e6d547c6dd6168d86d36b31
Van Luu <thanhvanluu.3199@gmail.com>
2024-10-16 00:28:06 +0700

    Update stylings for About dialog

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30">
+    <div class="self-start max-w-screen-md p-4 mt-32 bg-white rounded-lg shadow-xl">
+      <slot />
+      <button class="px-6 py-2 mt-6 mb-2 text-white rounded-full float-end bg-weather-primary hover:bg-weather-secondary">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -11,10 +11,56 @@
         <i class="text-xl duration-150 cursor-pointer hover:text-weather-secondary fa-solid fa-circle-info"></i>
         <i class="text-xl duration-150 cursor-pointer hover:text-weather-secondary fa-solid fa-plus"></i>
       </div>
+
+      <BaseModal class="text-black">
+        <div class="pb-3 mb-3 border-b-2">
+          <h1 class="my-2 text-2xl font-bold">About</h1>
+          <p>The Local Weather allows you to track the current and future weather of cities of your choosing.</p>
+        </div>
+        
+        <div class="mt-3 mb-3">
+          <h3 class="mb-2 text-lg font-medium">How it works</h3>
+          <div class="flex flex-row" v-for="rule in rules" :key="rule.id">
+            <div class="w-5 h-5 mt-1">
+              <component class="w-4 h-4 fill-weather-secondary" :is="rule.icon" />
+            </div>
+            <span class="ms-2">{{ rule.text }}</span>
+          </div>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
   import { RouterLink } from "vue-router";
+  import BaseModal from "./BaseModal.vue";
+  import Search from '@/assets/icons/Search.vue';
+  import Select from '@/assets/icons/Select.vue';
+  import Tracking from '@/assets/icons/Tracking.vue';
+  import Remove from "@/assets/icons/Remove.vue";
+
+  const rules = [
+    {
+      id: 'searching',
+      icon: Search,
+      text: 'Search for your city by entering the name into the search bar.'
+    },
+    {
+      id: 'selecting',
+      icon: Select,
+      text: 'Select a city within the results, this will take you to the current weather for your selection.'
+    },
+    {
+      id: 'tracking',
+      icon: Tracking,
+      text: 'Track the city by clicking on the "+" icon in the top right. This will save the city to view at a later time on the home page.'
+    },
+    {
+      id: 'removing',
+      icon: Remove,
+      text: 'If you no longer wish to track a city, simply select the city within the home page. At the bottom of the page, there will be an option to delete the city.'
+    },
+  ];
+
 </script>
