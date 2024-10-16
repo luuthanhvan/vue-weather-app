f7cd3af5e09133e80d106368af672325ece732c5
Van Luu <thanhvanluu.3199@gmail.com>
2024-10-16 20:47:39 +0700

    Export multiple icon components for easy reuse

---
+++ src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import Remove from "./Remove.vue";
+import Search from "./Search.vue";
+import Select from "./Select.vue";
+import Tracking from "./Tracking.vue";
+
+export {
+  Remove,
+  Search,
+  Select,
+  Tracking,
+}
